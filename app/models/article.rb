08d7322aa00a3cbc56a3219f0744caeeacb666f6
Takao <k.takao@neogenia.co.jp>
2023-02-20 23:48:33 +0900

    docs: exec annotate

--- app/models/article.rb
+++ app/models/article.rb
@@ -6,3 +6,15 @@ class Article < ApplicationRecord
   validates :title, presence: true
   validates :body, presence: true, length: { minimum: 10 }
 end
+
+# == Schema Information
+#
+# Table name: articles
+#
+#  id         :bigint           not null, primary key
+#  body       :text
+#  status     :string
+#  title      :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
--- test/models/article_test.rb
+++ test/models/article_test.rb
@@ -5,3 +5,15 @@ class ArticleTest < ActiveSupport::TestCase
   #   assert true
   # end
 end
+
+# == Schema Information
+#
+# Table name: articles
+#
+#  id         :bigint           not null, primary key
+#  body       :text
+#  status     :string
+#  title      :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
